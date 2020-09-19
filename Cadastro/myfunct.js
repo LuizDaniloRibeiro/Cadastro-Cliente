46f0d267ed8f0724e98bb53e0916243a769f2a29
Luiz Danilo <luizdanilor@gmail.com>
2020-09-18 23:35:15 -0300

    criando mascara

--- Cadastro/myfunct.js
+++ Cadastro/myfunct.js
@@ -23,24 +23,36 @@ var clientes = [];
 function Cadastrar()
 {
 
+    
+
     let cliente = new Cliente();
 
-    cliente.nome       = document.getElementById("IdNome").value;
-    cliente.email      = document.getElementById("IdEmail").value;
-    cliente.cpf        = parseInt(document.getElementById("IdCPF").value);
-    cliente.telefone   = parseInt(document.getElementById("IdTelefone").value);
-    cliente.cep        = parseInt(document.getElementById("IdCEP").value);
-    cliente.logradouro = document.getElementById("IdLogradouro").value;
-    cliente.numero     = parseInt(document.getElementById("IdNumero").value);
-    cliente.bairro     = document.getElementById("IdBairro").value;
-    cliente.cidade     = document.getElementById("IdCidade").value; 
-    cliente.estado     = document.getElementById("IdEstado").value;
+    if(cliente == null)
+    {
+        alert("Preenche todos os campos")
+    } 
+    else
+    {
+        cliente.nome       = document.getElementById("IdNome").value;
+        cliente.email      = document.getElementById("IdEmail").value;
+        cliente.cpf        = parseInt(document.getElementById("IdCPF").value);
+        cliente.telefone   = parseInt(document.getElementById("IdTelefone").value);
+        cliente.cep        = parseInt(document.getElementById("IdCEP").value);
+        cliente.logradouro = document.getElementById("IdLogradouro").value;
+        cliente.numero     = parseInt(document.getElementById("IdNumero").value);
+        cliente.bairro     = document.getElementById("IdBairro").value;
+        cliente.cidade     = document.getElementById("IdCidade").value; 
+        cliente.estado     = document.getElementById("IdEstado").value;
+
+        
+        clientes.push(cliente)
+        AddCliente(cliente);
+        Limpar();
+    }
+
 
     
-    clientes.push(cliente)
-    AddCliente(cliente);
-    Limpar();
-    alert("Cadastrado com sucesso!");
+    
 }
 
 
@@ -73,8 +85,43 @@ function AddCliente(cliente)
     cellCidade.innerHTML     = cliente.cidade;
     cellEstado.innerHTML     = cliente.estado; 
 
+
+    AddBtnRemover(cellBtnRemover, cliente);
+
 }
 
+
+//Função remover
+
+function RemoverCliente(rowIndex, cliente){
+
+    document.getElementById("IdTabelaCliente").deleteRow(rowIndex); 
+
+}
+
+
+
+
+
+
+//Botão remover
+function AddBtnRemover(cell, cliente){
+
+    let BtnRemover           = document.createElement("BUTTON");
+    BtnRemover.innerHTML     = "Excluir";
+    BtnRemover.name          = "BtnRemover";
+    BtnRemover.class         = "btn btn-primary";
+    BtnRemover.onclick       = function(){
+
+        RemoverCliente(cell.parentNode.rowIndex, cliente)
+
+    }
+
+    cell.appendChild(BtnRemover);
+
+    cell.appendChild(document.createTextNode(" "));
+
+}
 
 
 
@@ -86,6 +133,57 @@ function Limpar(){
 
 }
 
+
+
+
+
+//mascaras para form
+
+const masks = {
+    cpf (value){
+        return value
+            .replace(/\D/g, '')
+            .replace(/(\d{3})(\d)/, '$1.$2')
+            .replace(/(\d{3})(\d)/, '$1.$2')
+            .replace(/(\d{3})(\d)/, '$1-$2')
+            .replace(/(-\d{2})\d+$/, '$1')
+    }
+}
+
+/*cnpj (value) {
+
+    return value
+    .replace(/\D/g, '')
+    .replace(/(\d{2})(\d)/, '$1.$2')
+    .replace(/(\d{3})(\d)/, '$1.$2')
+    .replace(/(\d{3})(\d)/, '$1/$2')
+    .replace(/(\d{4})(\d)/, '$1-$2')
+    .replace(/(-\d{2})\d+$/, '$1')
+
+}*/
+
+
+telefone (value){
+    return value
+    .replace(/\D/g, '')
+    .replace(/(\d{2})(\d)/, '($1) $2')
+}
+
+
+
+
+
+
+
+
+
+document.querySelectorAll('input').forEach(($input) => {
+    const field = $input.dataset.js
+
+    $input.addEventListener('input', (e) => {
+        e.target.value = masks[field](e.target.value)
+    }, false)
+})
 
 
 
